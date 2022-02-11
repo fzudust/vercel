cf0c5606f29808f3753cdefd86e33c722352a8fc
zhengjunzhe <13723751903@139.com>
2022-02-11 15:59:08 +0800

    fix: 36kr请求失败

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -7,6 +7,9 @@ const httpsAgent = new https.Agent({ keepAlive: true })
 const axiosConfig: AxiosRequestConfig = {
   responseType: 'document',
   httpsAgent,
+  headers: {
+    'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36",
+  }
 };
 const instance = axios.create(axiosConfig);
 

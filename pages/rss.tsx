5f5ee7016e86e1b2d32fc1b86440e10fba9071b6
zhengjunzhe <13723751903@139.com>
2022-05-23 10:01:26 +0800

    优化缓存

--- pages/rss.tsx
+++ pages/rss.tsx
@@ -528,7 +528,7 @@ const RssReader: NextPage = () => {
     window.addEventListener('load', () => {
       // Is service worker available?
       if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js').then(() => {
+        navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(() => {
           console.log('Service worker registered!');
         }).catch((error) => {
           console.warn('Error registering service worker:');
--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.onerror = function (e) {
   console.error(e);
 }
-self.addEventListener('push', function (event) {
+self.addEventListener('push', (event) => {
 
   const notificationData = event.data.json();
   console.log('收到推送', notificationData);
@@ -9,7 +9,7 @@ self.addEventListener('push', function (event) {
   // 弹消息框
   event.waitUntil(self.registration.showNotification(title, notificationData));
 });
-self.addEventListener('notificationclick', function (event) {
+self.addEventListener('notificationclick', (event) => {
   console.log('点击消息框');
   const notification = event.notification;
   notification.close();
@@ -22,6 +22,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(self.skipWaiting())
 });
 
+// https://www.jsdelivr.com/
 const version = '6.5.3';
 const cdn = `https://cdn.jsdelivr.net/npm/`;
 const swjs = `${cdn}workbox-sw@${version}/build/workbox-sw.js`;
@@ -38,7 +39,7 @@ workbox.core.setCacheNameDetails({
   prefix: 'vercel',
   suffix: 'v1'
 });
-const otherjs = ['workbox-routing', 'workbox-strategies'];
+const otherjs = ['workbox-routing', 'workbox-strategies', 'workbox-cacheable-response'];
 otherjs.forEach(function (e) {
   const url = `${cdn}${e}@${version}/build/${e}.${dev ? 'dev' : 'prod'}.js`;
   self.importScripts(url);
@@ -48,15 +49,20 @@ const filter = (url, arr) => {
   const filter = arr.filter(regexp => url.href.match(regexp));
   return filter.length > 0
 }
-workbox.routing.registerRoute(
-  ({
-    url
-  }) => {
-    return filter(url, [
-      /.+(?:\.js|\.css)$/ig,
-      /.+(?:\.ico|\.svg|\.woff|\.png|\.jpg|\.jpeg|\.webp)$/ig,
-      /.+iframe\.(html|css).*/ig,
-    ])
-  },
-  new workbox.strategies.CacheFirst(),
-);
+
+const { registerRoute, Route } = workbox.routing;
+const { CacheFirst } = workbox.strategies;
+const { CacheableResponsePlugin } = workbox.cacheableResponse;
+
+const cacheRoute = new Route(({ url }) => {
+  return filter(url, [
+    /.+(?:\.js|\.css)$/ig,
+    /.+(?:\.ico|\.svg|\.woff|\.png|\.jpg|\.jpeg|\.webp)$/ig,
+    /.+iframe\.html/ig,
+  ])
+}, new CacheFirst({
+  cacheName: 'cache',
+  plugins: [new CacheableResponsePlugin({ statuses: [0, 200] })],
+}));
+
+registerRoute(cacheRoute);
